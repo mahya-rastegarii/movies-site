dc86631dfa384cffb4633e5074f058f16439fe6f
mahya rastegari <mahyarastegarii@gmail.com>
2024-09-19 20:11:04 +0330

    complete Home page Design

--- app/page.js
+++ app/page.js
@@ -1,9 +1,47 @@
+"use client";
 
+import { Images } from "@/app/fetch/slider3D-data";
+import { useEffect, useState } from "react";
+
+import Slider3D from "./components/slider/slider3D/slider";
+import SliderContainer from "./components/slider/slider3D/slider-container";
+import AdvancedSearchBox from "./components/search/advanced-search-box";
+import Container from "./components/searchBox-sidebar/container";
+import SimpleSlider from "./components/slider/simple-slider/slider";
 
 export default function Home() {
+  const [slideInfo, setSlideInfo] = useState();
+
+  const slider = [
+    {
+      delay: 2500,
+      title: " جدیدترین فیلم ها",
+      // data: newMovie,
+      data: Images,
+
+    },
+    {
+      delay: 2500,
+      title: " جدیدترین سریال ها",
+      // data: NewSerial,
+      data: Images,
+    },
+  ];
+  useEffect(()=> {
+    setSlideInfo(Images[0])
+  }, [])
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h2> nextJS </h2>
+    <main className=" w-full flex flex-col justify-center items-center">
+      <SliderContainer slideInfo={slideInfo}>
+        <Slider3D setSlideInfo={setSlideInfo} />
+      </SliderContainer>
+    
+      <Container>
+         {slider.map((slider, index) => (
+              <SimpleSlider {...slider} key={index} />
+            ))}
+      </Container>
     </main>
   );
 }
